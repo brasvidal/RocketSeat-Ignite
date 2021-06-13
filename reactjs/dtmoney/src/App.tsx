598699c8b964a5baba5ae9f49e6a42eb72acb048
Marcos <marcosvidal@outlook.com>
2021-06-13 20:36:25 -0300

    Adicionado modal + estilizaçao modal

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -3,12 +3,45 @@ import { Dasboard } from "./components/Dashboard/Index";
 import { Header } from "./components/Header/Index";
 import { TransactionsTable } from "./components/TransactionsTable/Index";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal/Index";
+import { useState } from "react";
+
+Modal.setAppElement('#root');
+  
+  
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+    
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-    <Header />
+
+    <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
     <Dasboard />
+
+    <NewTransactionModal
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+    />
+
     <TransactionsTable />
+
+
+    
+    
+    
+    
+    
+    
+    
     <GlobalStyle />
 
     </>
--- reactjs/dtmoney/src/components/Header/Index.tsx
+++ reactjs/dtmoney/src/components/Header/Index.tsx
@@ -1,27 +1,16 @@
-import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
-import Modal from 'react-modal';
 import { Container, Content } from './styles';
-
-export function Header(){
-    
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-    
-    function handleOpenNewTransactionModal(){
-        setIsNewTransactionModalOpen(true);
-    }
-    function handeCloseNewTransactionModal(){
-        setIsNewTransactionModalOpen(false);
-    }
-    
-    
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void;
+}
+export function Header({ onOpenNewTransactionModal }: HeaderProps){
     
     return(
     <Container>
         <Content>
             <img src={logoImg} alt="dt money" />
             <button type="button" 
-            onClick={handleOpenNewTransactionModal}>
+            onClick={onOpenNewTransactionModal}>
                 Nova transação
             </button>
         </Content>
